7ae132e079b2d5d68264d226e7056c0f50fddfb0
Spencer Smith <stix1919@hotmail.com>
2018-08-22 09:05:28 -0500

    SquareNumbers problem added

--- toys.js
+++ toys.js
@@ -5,3 +5,19 @@
 function vowelCounter(string) {
     return string.match(/[aeiou]/ig).length
 }
+
+
+// SQUARING NUMBERS
+
+// Write a function to square every digit of an integer.
+
+// Note: The function should accept an integer and return an integer
+
+function squareNumbers(num) {
+    let numArr = (''+num).split('')
+
+    let newArr = numArr.map((a) => {
+        return a*a
+    })
+    return Number(newArr.join(''))
+}

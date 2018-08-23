76f9ee08f5d4d9479dc3b5272c49d3099d339dcd
Spencer Smith <stix1919@hotmail.com>
2018-08-23 09:07:57 -0500

    Did fibonacci!

--- toys.js
+++ toys.js
@@ -21,3 +21,23 @@ function squareNumbers(num) {
     })
     return Number(newArr.join(''))
 }
+
+
+//FIBONACCI
+
+// Create an array the size of n with the values of the fibannaci sequence 
+//and reverse the order in which the sequence is displayed.
+
+function fibonacci(number) {
+    let fibArr = []
+
+    for(let i = 0; i < number; i++){
+        if(!fibArr[0] || !fibArr[1]){
+            fibArr.push(1)
+        } else fibArr.push(fibArr[i-2] + fibArr[i-1])
+    }
+    return fibArr.reverse()
+}
+
+
+

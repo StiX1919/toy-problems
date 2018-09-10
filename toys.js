a3a57053ea4163e5093b361b9f40cbf40080d713
Spencer Smith <stix1919@hotmail.com>
2018-09-10 10:15:59 -0500

    total sum between nums

--- toys.js
+++ toys.js
@@ -159,3 +159,19 @@ function readableTime(sec, cb) {
           }
         }
     }
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between 
+//     including them too and return it. If the two numbers are equal return a or b.  
+//     Note: a and b are not ordered!
+
+function GetSum( a,b ) {
+    let min = Math.min(a,b)
+    let max = Math.max(a,b)
+    
+    let total = 0
+    
+    for(let i = min; i <= max; i++){
+      total += i
+    }
+    return total
+  }
